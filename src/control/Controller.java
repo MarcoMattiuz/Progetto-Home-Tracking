2dc2e65fa3381c8eb5580f4e4ebdd7302d1eeb15
Davide Biasuzzi <biasuzzi.davide@gmail.com>
2022-04-20 23:08:57 +0200

    Aggiornamento GUI
    Iniziata la GUI

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -9,26 +9,36 @@ import model.Contract;
 import model.Home;
 import model.Room;
 import view.Main_Panel;
+import view.*;
 
 public class Controller extends Thread implements ActionListener {
 
 	private Home home;
+	private Window window;
 	private Semaphore ligthSem; ////////////////
 	private Semaphore fridgeSem;
 
 	public Controller(Home home) {
 		this.home = home;
-		this.ligthSem = new Semaphore(1);
-		this.fridgeSem = new Semaphore(1);
-		home.addRoom("1", new Room("soggiorno"));
-		home.getRoom("1").addDevice(new Ligths("luce", 1, new Consume(0.1, 0, 0, home.getContract()), home, ligthSem));
-		home.getRoom("1")
-				.addDevice(new Fridges("frigo", 2, new Consume(0.4, 0, 0, home.getContract()), home, fridgeSem));
+		
+//		this.ligthSem = new Semaphore(1);
+//		this.fridgeSem = new Semaphore(1);
+//		home.addRoom("1", new Room("soggiorno"));
+//		home.getRoom("1").addDevice(new Ligths("luce", 1, new Consume(0.1, 0, 0, home.getContract()), home, ligthSem));
+//		home.getRoom("1")
+//				.addDevice(new Fridges("frigo", 2, new Consume(0.4, 0, 0, home.getContract()), home, fridgeSem));
+	}
+	
+	public void setWindow(Window win) {
+		this.window = win;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(window.getContentPane() instanceof NoHousePanel) {
+			if(e.getSource()==((NoHousePanel) window.getContentPane()).getNameBtn()) {
+				window.setHomePanel();
+			}
+		}
 	}
 }
--- src/view/Window.java
+++ src/view/Window.java
@@ -1,7 +1,6 @@
 package view;
 
 import java.awt.*;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,81 +19,132 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.management.JMRuntimeException;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Window extends JFrame implements ActionListener,WindowListener{
-
+	
+	private final String title;
 	private Boolean opened;					// Se è true la finestra è già stata aperta una volta
+	private Boolean ft1;
+	private Boolean ft2;	
 	private JPanel contentPane;
+	private JPanel homePanel;
+	private JPanel contractPanel;
 	private JButton home_btn;
 	private Controller controller;
 	private JButton rooms_btn;
 	private JButton exit_btn;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_3;
+	private JButton btnNewButton_4;
+	private JMenu menu;
 	
 	public Window(Controller controller) {
+		title="HOME TRACKING V1";
+		this.controller=controller;
+		setVisible(true);
 		opened=false;
+		ft1=ft2=true;
 		this.controller = controller;
 		setResizable(false);
 		setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		setType(Type.POPUP);
 		setBackground(Color.WHITE);
-		setBounds(100, 100, 466, 527);
+		setBounds(500, 500, 466, 307);
 		addWindowListener(this);
 		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBackground(Color.WHITE);
+		menuBar.setBackground(SystemColor.menu);
 		setJMenuBar(menuBar);
-		
-		JMenu mnNewMenu = new JMenu("Menu");
-		mnNewMenu.setVerticalAlignment(SwingConstants.BOTTOM);
-		mnNewMenu.setHorizontalAlignment(SwingConstants.TRAILING);
-		mnNewMenu.setBackground(Color.DARK_GRAY);
-		menuBar.add(mnNewMenu);
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		menu = new JMenu("Menu");
+		menu.setVerticalAlignment(SwingConstants.BOTTOM);
+		menu.setHorizontalAlignment(SwingConstants.TRAILING);
+		menu.setBackground(Color.DARK_GRAY);
+		menuBar.add(menu);
 		
 		home_btn = new JButton("Home");
+		home_btn.setMaximumSize(new Dimension(70, 23));
+		home_btn.setMinimumSize(new Dimension(70, 23));
 		home_btn.addActionListener((ActionListener) this);
-		mnNewMenu.add(home_btn);
+		menu.add(home_btn);
 		
 		rooms_btn = new JButton("Rooms");
-		mnNewMenu.add(rooms_btn);
+		rooms_btn.setMinimumSize(new Dimension(70, 23));
+		rooms_btn.setMaximumSize(new Dimension(70, 23));
+		menu.add(rooms_btn);
 		rooms_btn.addActionListener(this);
 		
-		exit_btn = new JButton("Exit");
-		mnNewMenu.add(exit_btn);
-		exit_btn.addActionListener(this);
-		contentPane = new homePanel(controller);
-		contentPane.setBackground(Color.WHITE);
-		setContentPane(contentPane);
-		setTitle("Home Tracking 1.0");
-		setVisible(true);
+		btnNewButton = new JButton("n");
+		btnNewButton.setMinimumSize(new Dimension(70, 23));
+		btnNewButton.setMaximumSize(new Dimension(70, 23));
+		menu.add(btnNewButton);
 		
+		btnNewButton_1 = new JButton("n");
+		btnNewButton_1.setMinimumSize(new Dimension(70, 23));
+		btnNewButton_1.setMaximumSize(new Dimension(70, 23));
+		menu.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton("n");
+		btnNewButton_2.setMinimumSize(new Dimension(70, 23));
+		btnNewButton_2.setMaximumSize(new Dimension(70, 23));
+		menu.add(btnNewButton_2);
+		
+		btnNewButton_3 = new JButton("n");
+		btnNewButton_3.setMinimumSize(new Dimension(70, 23));
+		btnNewButton_3.setMaximumSize(new Dimension(70, 23));
+		menu.add(btnNewButton_3);
+		
+		btnNewButton_4 = new JButton("n");
+		btnNewButton_4.setMinimumSize(new Dimension(70, 23));
+		btnNewButton_4.setMaximumSize(new Dimension(70, 23));
+		menu.add(btnNewButton_4);
+		
+		exit_btn = new JButton("Exit");
+		exit_btn.setMaximumSize(new Dimension(70, 70));
+		menu.add(exit_btn);
+		exit_btn.addActionListener(this);
+		setTitle(title);
+		homePanel=new homePanel(controller);
+		contractPanel= new contractPanel(controller);
+		menu.hide();
+	}
+	
+	public void setController(Controller controller) {
+		throw new IllegalStateException();
 	}
 	
 	@Override
 	public void windowOpened(WindowEvent e) {
-		// TODO Auto-generated method stub
-		if(!opened) {
-			opened=true;
-			
-			setContentPane(new homePanel(controller));
-		}
-		
+		setContentPane(new NoHousePanel(controller));
+		((NoHousePanel) getContentPane()).setTitleName(JOptionPane.showInputDialog("Welcome, please enter your name here"));
 	}
 
 	@Override
 	public void windowClosing(WindowEvent arg0) {
-		
+		System.exit(1);
 	}
 
 	@Override
 	public void windowClosed(WindowEvent e) {
-		
+
 	}
 
 	@Override
 	public void windowIconified(WindowEvent e) {
 		if(JOptionPane.showConfirmDialog(this, "Desideri uscire?")==JOptionPane.YES_OPTION)
-			System.exit(0); else setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+			System.exit(1); else setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 	}
 
 	@Override
@@ -103,9 +153,7 @@ public class Window extends JFrame implements ActionListener,WindowListener{
 	}
 
 	@Override
-	public void windowActivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void windowActivated(WindowEvent e) {		
 	}
 
 	@Override
@@ -113,20 +161,47 @@ public class Window extends JFrame implements ActionListener,WindowListener{
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==home_btn) {
-			setBounds(100, 100, 466, 449);
-			contentPane.setVisible(true);
-			setContentPane(contentPane);
+			setHomePanel();
 		}else if(e.getSource()==rooms_btn) {
-			contentPane.setVisible(false);
-			setContentPane(new contractPanel());
-			setBounds(100, 100, 400, 400);
+			setBounds(500, 500, 466, 307);
+			//getContentPane().setVisible(false);
+			setContentPane(contractPanel);
+			menu.setPopupMenuVisible(false);menu.setSelected(false);
 		}else if(e.getSource()==exit_btn) {
-			System.exit(EXIT_ON_CLOSE);
+//			System.exit(EXIT_ON_CLOSE);
+			windowIconified(null);
 		}
+	}
+	
+	public void setDisableAllMenuButtons(Boolean bool) {
+		if(bool) {
+			btnNewButton.setVisible(false);
+		}
+	}
+	
+	public void setHomePanel() {
+		menu.show();
+		menu.setPopupMenuVisible(true);
+		menu.setPopupMenuVisible(false);
+		setBounds(500, 500, 466, 307);
+		//getContentPane().setVisible(false);
+		setContentPane(homePanel);
+	}
+	
+	public void setContractPanel() {
+		setBounds(500, 500, 466, 307);
+		getContentPane().setVisible(false);
+		setContentPane(new contractPanel(controller));
+	}
+	
+	@Override
+	public Container getContentPane() {
+		// TODO Auto-generated method stub
+		return super.getContentPane();
 	}
 	
 }
