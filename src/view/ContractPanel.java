800f8cee780edf2cdfe396c21ad5e342acef0986
Davide Biasuzzi <biasuzzi.davide@gmail.com>
2022-04-21 17:18:58 +0200

    Aggiornamento GUI
    Finita la parte di GUI riguardante la generazione della casa

---
+++ src/view/ContractPanel.java
@@ -0,0 +1,219 @@
+package view;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import control.Controller;
+
+import java.awt.Font;
+import javax.swing.SpringLayout;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JToolBar;
+import javax.swing.JTabbedPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JInternalFrame;
+import javax.swing.JDesktopPane;
+import javax.swing.Box;
+import java.awt.Component;
+import javax.swing.BoxLayout;
+import javax.swing.JProgressBar;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.border.BevelBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.JSpinner;
+import javax.swing.JSplitPane;
+import javax.swing.JRadioButton;
+import java.awt.Cursor;
+import javax.swing.SpinnerNumberModel;
+import java.awt.Dimension;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.DebugGraphics;
+import javax.swing.JSlider;
+
+public class ContractPanel extends JPanel implements DocumentListener{
+
+	/**
+	 * Create the panel.
+	 */
+	Controller controller;
+	private JTextField contractNameTfd;
+	private JTextField ePriceTfd;
+	private JTextField gPriceTfd;
+	private JTextField wPriceTfd;
+	private JButton backBtn;
+	private JButton proceedBtn;
+	private JProgressBar progressBar;
+	private int progressState;
+	
+	public ContractPanel(Controller controller) {
+		setSize(new Dimension(430, 244));
+		this.controller=controller;
+		setLayout(new BorderLayout(0, 0));
+		
+		JLabel titleLabel = new JLabel("CREATING A NEW HOME -> CONTRACT");
+		titleLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		add(titleLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		panel.setSize(new Dimension(466, 400));
+		add(panel, BorderLayout.CENTER);
+		panel.setLayout(new MigLayout("", "[82.00][41.00][][349.00,grow]", "[30.00][30.00][30.00:30.00][30.00][10.00][:37.00:30px,grow][10.00][]"));
+		
+		JLabel contractNameLabel = new JLabel("Contract Name");
+		panel.add(contractNameLabel, "cell 0 0");
+		
+		contractNameTfd = new JTextField();
+		panel.add(contractNameTfd, "cell 3 0,growx,aligny center");
+		contractNameTfd.setColumns(10);
+		
+		JLabel ePriceLabel = new JLabel("Electricity Price");
+		panel.add(ePriceLabel, "cell 0 1");
+		
+		ePriceTfd = new JTextField();
+		panel.add(ePriceTfd, "flowx,cell 3 1,growx");
+		ePriceTfd.setColumns(10);
+		
+		JLabel gPriceLabel = new JLabel("Gas Price");
+		panel.add(gPriceLabel, "cell 0 2");
+		
+		gPriceTfd = new JTextField();
+		panel.add(gPriceTfd, "flowx,cell 3 2,growx");
+		gPriceTfd.setColumns(10);
+		
+		JLabel wPriceLabel = new JLabel("Water Price");
+		panel.add(wPriceLabel, "cell 0 3");
+		
+		wPriceTfd = new JTextField();
+		panel.add(wPriceTfd, "flowx,cell 3 3,growx");
+		wPriceTfd.setColumns(10);
+		
+		progressBar = new JProgressBar();
+		panel.add(progressBar, "cell 0 7 4 1,growx");
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setDividerSize(0);
+		splitPane.setDebugGraphicsOptions(DebugGraphics.NONE_OPTION);
+		splitPane.setBorder(null);
+		splitPane.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		panel.add(splitPane, "cell 0 5 4 1,alignx center,growy");
+		
+		backBtn = new JButton("Back");
+		backBtn.setMinimumSize(new Dimension(71, 23));
+		backBtn.setMaximumSize(new Dimension(71, 23));
+		splitPane.setLeftComponent(backBtn);
+		
+		proceedBtn = new JButton("Proceed");
+		splitPane.setRightComponent(proceedBtn);
+		
+		JLabel lblNewLabel_4 = new JLabel("\u20AC/kWh");
+		panel.add(lblNewLabel_4, "cell 3 1,alignx trailing");
+		
+		JLabel lblNewLabel_5 = new JLabel("\u20AC/Smc");
+		lblNewLabel_5.setMinimumSize(new Dimension(31, 14));
+		lblNewLabel_5.setMaximumSize(new Dimension(31, 14));
+		panel.add(lblNewLabel_5, "cell 3 2,alignx trailing");
+		
+		JLabel lblNewLabel_6 = new JLabel("\u20AC/mq");
+		lblNewLabel_6.setMinimumSize(new Dimension(31, 14));
+		lblNewLabel_6.setMaximumSize(new Dimension(31, 14));
+		panel.add(lblNewLabel_6, "cell 3 3,alignx trailing");
+		setListener();
+	}
+	
+	public void setListener() {
+		contractNameTfd.getDocument().addDocumentListener(this);
+		ePriceTfd.getDocument().addDocumentListener(this);
+		gPriceTfd.getDocument().addDocumentListener(this);
+		wPriceTfd.getDocument().addDocumentListener(this);
+		backBtn.addActionListener(controller);
+		proceedBtn.addActionListener(controller);
+	}
+	
+	public void setProgressBar(int progressState) {
+		progressBar.setValue(progressState);
+		this.progressState=progressState;
+	}
+	
+	public int getIndexProgressBar() {
+		return progressState;
+	}
+	
+	private void updateProgressBar(int i) {
+		if(i>=0) {
+			progressState+=13;
+		}else {
+			progressState-=13;
+			if(progressState<0) {
+				progressState=0;
+			}
+		}
+		progressBar.setValue(progressState);
+	}
+
+
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		if(e.getDocument()==contractNameTfd.getDocument()) {
+			if(contractNameTfd.getText().length()==1) {
+				updateProgressBar(1);	
+			}
+		}else if(e.getDocument()==ePriceTfd.getDocument()) {
+			if(ePriceTfd.getText().length()==1) {
+				updateProgressBar(1);	
+			}
+		}else if(e.getDocument()==gPriceTfd.getDocument()) {
+			if(gPriceTfd.getText().length()==1) {
+				updateProgressBar(1);	
+			}
+		}else if(e.getDocument()==wPriceTfd.getDocument()) {
+			if(wPriceTfd.getText().length()==1) {
+				updateProgressBar(1);	
+			}
+		}	
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		if(e.getDocument()==contractNameTfd.getDocument()) {
+			if(contractNameTfd.getText().compareTo("")==0) {
+				updateProgressBar(-1);	
+			}
+		}else if(e.getDocument()==ePriceTfd.getDocument()) {
+			if(ePriceTfd.getText().compareTo("")==0) {
+				updateProgressBar(-1);	
+			}
+		}else if(e.getDocument()==gPriceTfd.getDocument()) {
+			if(gPriceTfd.getText().compareTo("")==0) {
+				updateProgressBar(-1);	
+			}
+		}else if(e.getDocument()==wPriceTfd.getDocument()) {
+			if(wPriceTfd.getText().compareTo("")==0) {
+				updateProgressBar(-1);	
+			}
+		}	
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public JButton getProceedBtn() {
+		return proceedBtn;
+	}
+	
+	public JButton getBackBtn() {
+		return backBtn;
+	}
+}
---
+++ src/view/HomePanel.java
@@ -0,0 +1,280 @@
+package view;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import control.Controller;
+import javax.swing.event.*;
+import javax.swing.plaf.ProgressBarUI;
+import java.awt.Font;
+import javax.swing.SpringLayout;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JToolBar;
+import javax.swing.JTabbedPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JInternalFrame;
+import javax.swing.JDesktopPane;
+import javax.swing.Box;
+import java.awt.Component;
+import javax.swing.BoxLayout;
+import javax.swing.JProgressBar;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.border.BevelBorder;
+import javax.swing.JSpinner;
+import javax.swing.JSplitPane;
+import javax.swing.JRadioButton;
+import java.awt.Cursor;
+import javax.swing.SpinnerNumberModel;
+import java.awt.Dimension;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.DebugGraphics;
+
+@SuppressWarnings("serial")
+
+public class HomePanel extends JPanel implements ActionListener,DocumentListener,ChangeListener{
+	
+	private int progressState;
+	private Controller controller;
+	private JTextField holderNameTfd;
+	private JButton yesBtn;
+	private JButton noBtn;
+	private JButton backBtn;
+	private JButton proceedBtn;
+	int flag;
+	boolean flag2;
+	private JTextField houseNameTfd;
+	private JProgressBar progressBar;
+	private JSpinner spinner;
+	
+	public HomePanel(Controller controller) {
+		progressState=0;
+		setSize(new Dimension(430, 244));
+		this.controller=controller;
+		setLayout(new BorderLayout(0, 0));
+		flag=1;
+		flag2=false;
+		JLabel titleLabel = new JLabel("CREATING A NEW HOME -> HOUSE");
+		titleLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		add(titleLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		panel.setSize(new Dimension(466, 400));
+		add(panel, BorderLayout.CENTER);
+		panel.setLayout(new MigLayout("", "[82.00][41.00][][349.00,grow]", "[30.00][30.00][30.00:30.00][30.00][10.00][:37.00:30px,grow][10.00][]"));
+		
+		JLabel holderNameLabel = new JLabel("Holder Name");
+		panel.add(holderNameLabel, "cell 0 0");
+		
+		holderNameTfd = new JTextField();
+		holderNameTfd.getDocument().addDocumentListener(this);
+		
+		panel.add(holderNameTfd, "cell 3 0,growx,aligny center");
+		holderNameTfd.setColumns(10);
+		
+		JLabel houseNameLabel = new JLabel("House Name");
+		panel.add(houseNameLabel, "cell 0 1");
+		
+		houseNameTfd = new JTextField();
+		panel.add(houseNameTfd, "cell 3 1,growx");
+		houseNameTfd.setColumns(10);
+		houseNameTfd.getDocument().addDocumentListener(this);
+		JLabel rnLabel = new JLabel("Rooms Number");
+		panel.add(rnLabel, "cell 0 2");
+		
+		spinner = new JSpinner();
+		panel.add(spinner, "cell 3 2,alignx left,aligny center");
+		
+		JLabel sPLabel = new JLabel("Solar Panels");
+		panel.add(sPLabel, "cell 0 3");
+		
+		yesBtn = new JButton("Yes");
+		panel.add(yesBtn, "flowx,cell 3 3");
+
+		
+		noBtn = new JButton("No");
+		panel.add(noBtn, "cell 3 3");
+		
+		progressBar = new JProgressBar();
+		panel.add(progressBar, "cell 0 7 4 1,growx");
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setDividerSize(0);
+		splitPane.setDebugGraphicsOptions(DebugGraphics.NONE_OPTION);
+		splitPane.setBorder(null);
+		splitPane.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		panel.add(splitPane, "cell 0 5 4 1,alignx center,growy");
+		backBtn = new JButton("Back");
+		backBtn.setMinimumSize(new Dimension(71, 23));
+		backBtn.setMaximumSize(new Dimension(71, 23));
+		splitPane.setLeftComponent(backBtn);
+		proceedBtn = new JButton("Proceed");
+		splitPane.setRightComponent(proceedBtn);
+		setListener();
+	}
+
+	private void setListener() {
+		yesBtn.addActionListener(controller);
+		yesBtn.addActionListener(this);
+		noBtn.addActionListener(controller);
+		noBtn.addActionListener(this);
+		backBtn.addActionListener(controller);
+		backBtn.addActionListener(this);
+		proceedBtn.addActionListener(controller);
+		proceedBtn.addActionListener(this);
+		spinner.addChangeListener(this);
+	}
+	
+	public JButton getYesBtn() {
+		return yesBtn;
+	}
+
+
+
+	public void setYesBtn(JButton yesBtn) {
+		this.yesBtn = yesBtn;
+	}
+
+
+
+	public JButton getNoBtn() {
+		return noBtn;
+	}
+
+
+
+	public void setNoBtn(JButton noBtn) {
+		this.noBtn = noBtn;
+	}
+
+
+
+	public JButton getBackBtn() {
+		return backBtn;
+	}
+
+
+
+	public void setBackBtn(JButton backBtn) {
+		this.backBtn = backBtn;
+	}
+
+
+
+	public JButton getProceedBtn() {
+		return proceedBtn;
+	}
+
+
+
+	public void setProceedBtn(JButton proceedBtn) {
+		this.proceedBtn = proceedBtn;
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==yesBtn) {
+			if(flag==0) {
+				noBtn.show();
+				flag=1;
+				yesBtn.setSelected(false);
+				updateProgressBar(-1);
+			}else {
+				noBtn.hide();
+				flag=0;
+				yesBtn.setSelected(true);
+				updateProgressBar(1);
+			}
+		}else if(e.getSource()==noBtn){
+			if(flag==0) {
+				yesBtn.show();
+				noBtn.setSelected(false);
+				updateProgressBar(-1);
+				flag=1;
+			}else {
+				yesBtn.hide();
+				updateProgressBar(1);
+				noBtn.setSelected(true);
+				flag=0;
+			}
+		}else if(e.getSource()==backBtn) {
+			//JOptionPane.showMessageDialog(this, "You can't go back!");
+		}
+	}
+	
+	private void updateProgressBar(int i) {
+		if(i>=0) {
+			progressState+=12;
+		}else {
+			progressState-=12;
+			if(progressState<0) {
+				progressState=0;
+			}
+		}
+		progressBar.setValue(progressState);
+	}
+	
+	public void setProgressBar(int progressState) {
+		progressBar.setValue(progressState);
+		this.progressState=progressState;
+	}
+	
+	public int getIndexProgressBar() {
+		return progressState;
+	}
+
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		if(e.getDocument()==holderNameTfd.getDocument()) {
+			if(holderNameTfd.getText().length()==1) {
+				updateProgressBar(1);	
+			}			
+		}else if(e.getDocument()==houseNameTfd.getDocument()) {
+			if(houseNameTfd.getText().length()==1) {
+				updateProgressBar(1);
+			}
+		}
+	}		
+	
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		if(e.getDocument()==holderNameTfd.getDocument()) {
+			if(holderNameTfd.getText().compareTo("")==0) {
+				updateProgressBar(-1);	
+			}			
+		}else if(e.getDocument()==houseNameTfd.getDocument()) {
+			if(houseNameTfd.getText().compareTo("")==0) {
+				updateProgressBar(-1);
+			}
+		}
+		
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		if(spinner.getValue().equals(0)) {
+			updateProgressBar(-1);
+			flag2=false;
+		}else {			
+			if(!flag2) {
+				updateProgressBar(1);	
+				flag2=true;
+			}
+		}
+	}
+}
