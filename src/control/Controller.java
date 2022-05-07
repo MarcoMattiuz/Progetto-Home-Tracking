9aec1d5276977248922600ac552e9069c1c8b582
Davide Biasuzzi <biasuzzi.davide@gmail.com>
2022-05-07 11:01:23 +0200

    Chiediamo all'utente quanta potenza vuole dai pannelli
    Bisogna vedere se funziona il doppio operatore ternario

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -292,7 +292,8 @@ public class Controller extends Thread implements ActionListener,ListSelectionLi
 		
 			// Fine Controlli su tutti i campi dati, si puï¿½ creare la casa adesso
 					if(fieldflag) { 
-						roomsNames=generateHouse((window.getHomePanel()).getRoomsNumber(),(((HomePanel) window.getHomePanel()).wantSolarPanels()==1),2); // TO-DO passare al metodo isSolar (se ci sono i pannelli) e solar (la potenza sviluppata) che va da 2 a 6
+						roomsNames=generateHouse((window.getHomePanel()).getRoomsNumber(),(((HomePanel) window.getHomePanel()).wantSolarPanels()==1),(((HomePanel) window.getHomePanel()).wantSolarPanels()==1)?(window.showBooleanErrorMessage(""
+								+ "Hai scelto di utilizzare i pannelli,\nla potenza base è di 3kw, desideri avere 6 kw?")?3:6):0); // TO-DO passare al metodo isSolar (se ci sono i pannelli) e solar (la potenza sviluppata) che va da 2 a 6
 						Collections.reverse(roomsNames);
 						roomsNamesReversed=roomsNames;
 						Collections.reverse(roomsNames);
@@ -366,6 +367,7 @@ public class Controller extends Thread implements ActionListener,ListSelectionLi
 			}
 		}
 	}
+	
 //		public void run() {
 //			while(true) {
 //				try {
