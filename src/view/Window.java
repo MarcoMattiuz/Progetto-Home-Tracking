1c4747e1246e4d4ac74c5ce800a6577ba3bf6b3b
Biasuzzi Davide <davidinobiasu19@gmail.com>
2022-05-09 12:37:56 +0200

    .
    .

--- src/view/Window.java
+++ src/view/Window.java
@@ -144,7 +144,7 @@ public class Window extends JFrame implements ActionListener, WindowListener {
 		try {
 		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
 		        if ("Nimbus".equals(info.getName())) {
-		            UIManager.setLookAndFeel(info.getClassName());
+		            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		            break;
 		        }
 		    }
